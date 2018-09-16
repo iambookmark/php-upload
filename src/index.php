0ae6e4051a1d0a43089d465304e51f02537654b9
iambookmark <iconv@hotmail.com>
2018-09-16 23:45:03 +0700

    upload file sample

---
+++ src/index.php
@@ -0,0 +1,28 @@
+<?php
+    if ($handle = opendir('uploads')) {
+        while (false !== ($entry = readdir($handle))) {
+            if ($entry != "." && $entry != "..") {
+                echo "$entry<br/>\n";
+            }
+        }
+        closedir($handle);
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<p></p>
+<form action="upload.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+</body>
+</html>
